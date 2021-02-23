09bbb2c0c479f561b7a37a98f32c08e8022066c1
krishna <krishna.nature91@gmail.com>
2021-02-23 22:56:08 +0000

    some examples on jsx template

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import './App.css';
 
 function App() {
+  const title = 'Welcome to the Inspirational quote app'
+  const likes = 50;
+  const quote = {quote: 'Be here', author: 'Luffy'}
+
   return (
     <div className="App">
       <div className="content">
-        <h1>App Components</h1>
+        <h1>{title}</h1>
+        <p>Liked {likes} times</p>
+        <p>{ quote.quote }</p>
+        <p>{ "Hello" }</p>
+        <p>{ [1,2,3] }</p>
+        <p>{ Math.random() * 100}</p>
       </div>
     </div>
   );
