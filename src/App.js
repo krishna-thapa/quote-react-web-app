7083f2e9b47473f59525c49d5988448851a4ad5b
krishna <krishna.nature91@gmail.com>
2021-02-23 23:11:11 +0000

    Added navbar

--- src/App.js
+++ src/App.js
@@ -1,19 +1,14 @@
 import './App.css';
+import Navbar from "./components/Navbar";
+import Home from "./components/Home";
 
 function App() {
-  const title = 'Welcome to the Inspirational quote app'
-  const likes = 50;
-  const quote = {quote: 'Be here', author: 'Luffy'}
 
   return (
     <div className="App">
+      <Navbar />
       <div className="content">
-        <h1>{title}</h1>
-        <p>Liked {likes} times</p>
-        <p>{ quote.quote }</p>
-        <p>{ "Hello" }</p>
-        <p>{ [1,2,3] }</p>
-        <p>{ Math.random() * 100}</p>
+        <Home />
       </div>
     </div>
   );
---
+++ src/components/Home.js
@@ -0,0 +1,11 @@
+const Home = () => {
+  const title = 'Welcome to the Inspirational quote app'
+
+  return ( 
+    <div className="home">
+      <h2>{ title }</h2>
+    </div>
+   );
+}
+ 
+export default Home;
