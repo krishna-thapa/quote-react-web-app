958a9ef91219a93154749f34f200727d6cdffb23
krishna <krishna.nature91@gmail.com>
2021-02-25 23:15:42 +0000

    Added list of quotes

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,37 @@
+import { useState } from "react";
+
 const Home = () => {
-  const title = 'Welcome to the Inspirational quote app'
+  const [quotes, setQuotes] = useState([
+    {
+      id: 1,
+      text: "Well begun is half done.",
+      author: "Aristotle"
+    },
+    {
+      id: 2,
+      text: "Life is a learning experience, only if you learn.",
+      author: "Yogi Berra"
+    },
+    {
+      id: 3,
+      text: "Self-complacency is fatal to progress.",
+      author: "Margaret Sangster"
+    },
+    {
+      id: 4,
+      text: "Peace comes from within. Do not seek it without.",
+      author: "Buddha"
+    }
+  ]);
 
   return ( 
     <div className="home">
-      <h2>{ title }</h2>
+      { quotes.map((quote) => (
+        <div className="quote-preview" key={ quote.id }>
+          <h2>{ quote.text }</h2>
+          <p>By: { quote.author }</p>
+        </div>
+      ))}
     </div>
    );
 }
