3344c7e4721bbe817ac726db8f3d435425e5a2dd
krishna <krishna.nature91@gmail.com>
2021-03-04 22:28:06 +0000

    Error handling done

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,26 +1,38 @@
 import { useState, useEffect } from "react";
-import { Spinner, Container } from "react-bootstrap";
+import { Alert, Spinner, Container } from "react-bootstrap";
 import QuoteList from "./QuoteList";
 
 const Home = () => {
   const [quotes, setQuotes] = useState(null);
   const [isPending, setIsPending] = useState(true);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
     setTimeout(() => {
-      fetch('http://localhost:8000/quotes')
+      fetch('http://localhost:8000/quotesd')
       .then(response => {
+        if(!response.ok) throw Error('Error response!');
         return response.json();
       })
       .then((data) => {
         setQuotes(data);
         setIsPending(false);
+        setError(null);
+      })
+      .catch(err => {
+        setIsPending(false);
+        setError(err.message);
       })
     }, 500); //To show the loading spinner
   }, []);
 
   return ( 
     <div className="home">
+      {error && 
+      <Alert variant='danger'>
+        Response from API: {error}
+      </Alert>
+      }
       {isPending && 
         <Container className="d-flex align-items-center justify-content-center" 
           style={{ minHeight: "10vh"}}>
