d67b701b78e8ab27dba49d5ca8b9da20fe4c7958
krishna <krishna.nature91@gmail.com>
2021-03-04 22:12:22 +0000

    Bootstrap is added with loading spinner

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,5 @@
 import { useState, useEffect } from "react";
+import { Spinner, Container } from "react-bootstrap";
 import QuoteList from "./QuoteList";
 
 const Home = () => {
@@ -6,7 +7,8 @@ const Home = () => {
   const [isPending, setIsPending] = useState(true);
 
   useEffect(() => {
-    fetch('http://localhost:8000/quotes')
+    setTimeout(() => {
+      fetch('http://localhost:8000/quotes')
       .then(response => {
         return response.json();
       })
@@ -14,11 +16,19 @@ const Home = () => {
         setQuotes(data);
         setIsPending(false);
       })
+    }, 500); //To show the loading spinner
   }, []);
 
   return ( 
     <div className="home">
-      {isPending && <div>Loading..</div>}
+      {isPending && 
+        <Container className="d-flex align-items-center justify-content-center" 
+          style={{ minHeight: "10vh"}}>
+          <Spinner animation="grow" role="status" variant="info">
+              <span className="sr-only">Loading...</span>
+          </Spinner>
+        </Container>
+        }
       {quotes && <QuoteList quotes = { quotes } 
         title = "Welcome to Inspirational quotes app" />}
     </div>
