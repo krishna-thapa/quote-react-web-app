ae4a6a20cbe4c4e23c6a8d7bb3427c683f942fc6
krishna <krishna.nature91@gmail.com>
2021-03-05 00:14:17 +0000

    Added react router link to navbar

--- src/components/QuoteNavbar.js
+++ src/components/QuoteNavbar.js
@@ -1,4 +1,5 @@
 import { Navbar, Nav } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 const QuoteNavbar = () => {
   return (
@@ -7,12 +8,12 @@ const QuoteNavbar = () => {
         <h1>Inspiration Quote App</h1>
       </div>
       <Navbar className="quote-navbar-content" bg="light" expand="sm">
-        <Navbar.Brand class="quote-navbar-text" href="/">Welcome</Navbar.Brand>
+        <Navbar.Brand class="quote-navbar-text" as={Link} to="/">Welcome</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="mr-auto links">
-            <Nav.Link href="/">Home</Nav.Link>
-            <Nav.Link href="#home">New Quote</Nav.Link>
+            <Nav.Link as={Link} to="/">Home</Nav.Link>
+            <Nav.Link as={Link} to="/create">New Quote</Nav.Link>
             <Nav.Link href="#link">About</Nav.Link>
           </Nav>
         </Navbar.Collapse>
