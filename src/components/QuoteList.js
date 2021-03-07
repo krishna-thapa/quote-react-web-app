699de2210fc7fe807942106040a760137f00f13e
krishna <krishna.nature91@gmail.com>
2021-03-07 11:14:13 +0000

    Added card group using react bootstrap

--- src/components/QuoteList.js
+++ src/components/QuoteList.js
@@ -1,16 +1,29 @@
-const QuoteList = ({quotes, title }) => {
-  
+import { CardColumns, Card } from "react-bootstrap";
+
+const QuoteList = ({ quotes, title }) => {
+
   return (
     <div className="quote-list">
-      <h2>{ title }</h2>
-      { quotes.map((quote) => (
-        <div className="quote-preview" key={ quote.id }>
-          <h2>{ quote.text }</h2>
-          <p>By: { quote.author }</p>
-        </div>
-      ))}
+      <h2>{title}</h2>
+      <CardColumns>
+        {quotes.map((quote) => (
+          <Card key={quote.id} border="dark">
+            <Card.Header>Quote</Card.Header>
+            <Card.Body>
+              <blockquote className="blockquote mb-0">
+                <p>
+                  {' '} {quote.text} {' '}
+                </p>
+                <footer className="blockquote-footer">
+                  <cite title="Source Title">{quote.author}</cite>
+                </footer>
+              </blockquote>
+            </Card.Body>
+          </Card>
+        ))}
+      </CardColumns>
     </div>
   );
 }
- 
+
 export default QuoteList;
