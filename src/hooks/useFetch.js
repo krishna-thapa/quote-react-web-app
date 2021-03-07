8b053f1af36fa1dcd4dce6c4d61d077821126ebf
krishna <krishna.nature91@gmail.com>
2021-03-07 09:42:38 +0000

    Clean up and fetch abort is added

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -9,24 +9,32 @@ const useFetch = (url) => {
   const [error, setError] = useState(null);
 
   useEffect(() => {
-    setTimeout(() => {
-      fetch(url)
-      .then(response => {
-        if(!response.ok) throw Error('Error response!');
-        return response.json();
-      })
-      .then((data) => {
-        setData(data);
-        setIsPending(false);
-        setError(null);
-      })
-      .catch(err => {
-        setIsPending(false);
-        setError(err.message);
-      })
-    }, 500); //To show the loading spinner
-  }, [url]);  // dependency on url, whenever url changes - renders
+    // to stop a fetch request when it's not needed
+    const abortCont = new AbortController();
 
+    setTimeout(() => {
+      fetch(url, { signal: abortCont.signal })
+        .then(response => {
+          if (!response.ok) throw Error('Error response!');
+          return response.json();
+        })
+        .then((data) => {
+          setData(data);
+          setIsPending(false);
+          setError(null);
+        })
+        .catch(err => {
+          if (err.name === 'AbortError') {
+            console.log('fetch aborted');
+          } else {
+            setIsPending(false);
+            setError(err.message);
+          }
+        })
+    }, 500); //To show the loading spinner
+    // clean-up function
+    return () => abortCont.abort();
+  }, [url]);  // dependency on url, whenever url changes - renders
   return { data, isPending, error }
 }
 
