82ae8ea1bab3dd58c20c5f44c228afb2a4b26f42
krishna <krishna.nature91@gmail.com>
2021-03-15 23:24:55 +0000

    Create quote page added

--- src/pages/Create.js
+++ src/pages/Create.js
@@ -1,9 +1,43 @@
+import { useState } from "react";
+import '../style/createQuote.css'
+
 const Create = () => {
-  return ( 
+  const [author, setAuthor] = useState('');
+  const [quoteBody, setQuoteBody] = useState('');
+  const [genre, setGenre] = useState('love');
+
+  return (
     <div className="create">
       <h2>Add a New Quote</h2>
+      <form>
+        <label>Author:</label>
+        <input
+          type="text"
+          required
+          value={author}
+          onChange={(e) => setAuthor(e.target.value)}
+        />
+        <label>Quote body:</label>
+        <textarea
+          required
+          value={quoteBody}
+          onChange={(e) => setQuoteBody(e.target.value)}
+        ></textarea>
+        <label>Genre:</label>
+        <select
+          value={genre}
+          onChange={(e) => setGenre(e.target.value)}
+        >
+          <option value="love">Love</option>
+          <option value="sad">Sad</option>
+          <option value="peace">Peace</option>
+          <option value="war">War</option>
+          <option value="family">family</option>
+          <option value="other">Other</option>
+        </select>
+      </form>
     </div>
-   );
+  );
 }
- 
+
 export default Create;
